cb8c7d46e46975a7918b037ce6583934c1bfaf37
daniil <danil-bryzgalov@mohawkcollege.ca>
2026-03-14 16:14:55 -0400

    Rewrite middleware client using aiomqtt. Much better

--- middleware-client.py
+++ middleware-client.py
@@ -1,43 +1,18 @@
 #!/home/daniil/miniconda3/envs/opcua/bin/python
-import asyncio
+import asyncio, aiomqtt
 import consts
-import logging
 import json
-import queue
-import paho.mqtt.client as mqtt
-from paho.mqtt.enums import CallbackAPIVersion
-
 from asyncua import Client, ua
 from asyncua.client.client import Subscription
 from asyncua.server import event_generator
 
 URL = f"opc.tcp://{consts.HOSTNAME}:4990/FactoryTalkLinxGateway/"
-NAMESPACE = 2
-
-data_queue = queue.Queue(maxsize=5)
-
-
-def on_connect(client, userdata, flags, reason_code, properties):
-    if reason_code == 0:
-        print("Connection to MQTT Server Successful")
-
-        client.subscribe(consts.DATA_TOPIC, qos=2)
-        client.subscribe(consts.CAMERA_TOPIC, qos=2)
-    else:
-        print(f"Connection failed: {reason_code.getName()}")
-
-def on_message(client, usedata, message, properties=None):
-    decoded_message = message.payload.decode('utf-8')
-    data = json.loads(decoded_message)
-    data_queue.put((message.topic, data))
 
 class SubHandler(object):
     """
     The SubscriptionHandler is used to handle the data that is received for the subscription.
     """
-    def __init__(self, client, idx, mqttc: mqtt.Client, ids) -> None:
-        self.client = client
-        self.idx = idx
+    def __init__(self, mqttc: aiomqtt.Client, ids) -> None:
         self.ids_node = ids
         self.mqttc = mqttc
 
@@ -48,103 +23,79 @@ class SubHandler(object):
         asyncio.create_task(self.handle_change(val))
 
     async def handle_change(self, val):
-        print(f"Raking in progress: {val[consts.LADLE_TILT_STATE_INDEX]}")
+        tilt_state = val[consts.LADLE_TILT_STATE_INDEX]
+        print(f"Raking in progress: {tilt_state}")
         process_ids = await self.ids_node.get_value()
         heat_id = int(process_ids[consts.HEAT_ID_INDEX])
-        if val[consts.LADLE_TILT_STATE_INDEX]: # raking has stated
+        if tilt_state: # raking has stated
             # Publish to raking event topic that the raking has started and is in process
-            event: dict = {"heat_id": heat_id, "state": val[consts.LADLE_TILT_STATE_INDEX]}
-            self.mqttc.publish(consts.EVENT_TOPIC, json.dumps(event), qos=2)
+            event: dict = {"heat_id": heat_id, "state": tilt_state}
+            await self.mqttc.publish(consts.EVENT_TOPIC, json.dumps(event), qos=2)
 
         else:
             # Publish to raking event topic that the raking has completed
-            event: dict = {"heat_id": heat_id, "state": val[consts.LADLE_TILT_STATE_INDEX]}
-            self.mqttc.publish(consts.EVENT_TOPIC, json.dumps(event), qos=2)
+            event: dict = {"heat_id": heat_id, "state": tilt_state}
+            await self.mqttc.publish(consts.EVENT_TOPIC, json.dumps(event), qos=2)
+
+async def handle_mqtt_messages(mqtt_client: aiomqtt.Client, opc_data_node, opc_flags_node):
+    await mqtt_client.subscribe(consts.DATA_TOPIC, qos=2)
+    await mqtt_client.subscribe(consts.CAMERA_TOPIC, qos=2)
+
+    async for message in mqtt_client.messages:
+        try:
+            mqtt_data = json.loads(message.payload.decode())
+            real_values = await opc_data_node.get_value()
+            bool_values = await opc_flags_node.get_value()
+
+            if message.topic.value == consts.DATA_TOPIC:
+                    # TODO: check that the heat id is the same as the one read and passed
+                    real_values[consts.HEAT_ID_INDEX] = float(mqtt_data["heat_id"])
+
+                    real_values[consts.TIME_INDEX] = float(mqtt_data["total_time_seconds"])
+                    real_values[consts.PULLS_INDEX] = float(mqtt_data["num_pulls"])
+                    real_values[consts.STEEL_START_INDEX] = mqtt_data["overall"]["steel_pct_start"]
+                    real_values[consts.STEEL_END_INDEX] = mqtt_data["overall"]["steel_pct_end"]
+                    real_values[consts.TOTAL_SLAG_START_INDEX] = mqtt_data["overall"]["total_slag_pct_start"]
+                    real_values[consts.TOTAL_SLAG_END_INDEX] = mqtt_data["overall"]["total_slag_pct_end"]
+                    real_values[consts.SOLID_SLAG_START_INDEX] = mqtt_data["overall"]["solid_slag_pct_start"]
+                    real_values[consts.SOLID_SLAG_END_INDEX] = mqtt_data["overall"]["solid_slag_pct_end"]
+                    real_values[consts.LIQUID_SLAG_START_INDEX] = mqtt_data["overall"]["liquid_slag_pct_start"]
+                    real_values[consts.LIQUID_SLAG_END_INDEX] = mqtt_data["overall"]["liquid_slag_pct_end"]
+                    real_values[consts.SLAG_START_INDEX] = mqtt_data["overall"]["slag_index_start"]
+                    real_values[consts.SLAG_END_INDEX] = mqtt_data["overall"]["slag_index_end"]
+                    print(f"Raking for heat id {mqtt_data["heat_id"]} took {mqtt_data["total_time_seconds"]}s and required {mqtt_data["num_pulls"]} pulls")
+
+            elif message.topic.value == consts.CAMERA_TOPIC:
+                    real_values[consts.CAMERA_TEMPERATURE_INDEX] = mqtt_data["temperature"]
+                    bool_values[consts.CAMERA_STATUS_INDEX] = mqtt_data["connected"]
+
+            await opc_data_node.set_value(real_values, ua.VariantType.Double)
+            await opc_flags_node.set_value(bool_values, ua.VariantType.Boolean)
+
+        except Exception as e:
+            print(f"Failed to process MQtt message or write to OPC: {e}")
 
 async def main():
-    # Creating mqtt_client instance
-    mqtt_client = mqtt.Client(CallbackAPIVersion.VERSION2)
+    print(f"Connecting to MQTT Broker at {consts.BROKER}")
+    async with aiomqtt.Client(consts.BROKER, consts.PORT) as mqtt_client:
 
-    mqtt_client.on_connect = on_connect
-    mqtt_client.on_message = on_message
+        print(f"Connecting to OPC Server at {URL} ...")
+        async with Client(url=URL) as opc_client:
 
-    mqtt_client.connect(consts.BROKER, consts.PORT)
+            states_node = opc_client.get_node("ns=2;s=[UA_server]OU_Server_IO.BOOL_Write")
+            ids_node = opc_client.get_node("ns=2;s=[UA_server]OU_Server_IO.REAL_Write")
+            data_node = opc_client.get_node("ns=2;s=[UA_server]OU_Server_IO.REAL_Read")
+            flags_node = opc_client.get_node("ns=2;s=[UA_server]OU_Server_IO.BOOL_Read") 
 
-    try:
-        mqtt_client.loop_start()
-
-        print(f"Connecting to {URL} ...")
-        async with Client(url=URL) as client:
-            idx = NAMESPACE
-
-            states = client.get_node("ns=2;s=[UA_server]OU_Server_IO.BOOL_Write")
-            ids = client.get_node("ns=2;s=[UA_server]OU_Server_IO.REAL_Write")
-            data = client.get_node("ns=2;s=[UA_server]OU_Server_IO.REAL_Read")
-            flags = client.get_node("ns=2;s=[UA_server]OU_Server_IO.BOOL_Read") 
-
-            handler = SubHandler(client, idx, mqtt_client, ids)
-
-            subscription = await client.create_subscription(500, handler)
-
-            await subscription.subscribe_data_change(states)
-
+            handler = SubHandler(mqtt_client, ids_node)
+            subscription = await opc_client.create_subscription(500, handler)
             print("Subscription active. Waiting for state changes...")
 
-            # Keep the connection alive
-            while True:
-                try:
-                    # Get value from mqtt
-                    try:
-                        raking_data = data_queue.get(block=False)
-                        cv_data: dict = {}
-                        camera_data: dict = {}
-                        if raking_data[0] == consts.DATA_TOPIC:
-                            cv_data = raking_data[1]
-                        elif raking_data[0] == consts.CAMERA_TOPIC:
-                            camera_data = raking_data[1]
-                    except queue.Empty:
-                        await asyncio.sleep(1)
-                        continue
-
-                    # Read current array
-                    real_values = await data.get_value()
-                    bool_values = await flags.get_value()
-
-                    # Update specific indices
-                    if cv_data:
-                        # TODO: check that the heat id is the same as the one read and passed
-                        real_values[consts.HEAT_ID_INDEX] = float(cv_data["heat_id"])
-
-                        real_values[consts.TIME_INDEX] = float(cv_data["total_time_seconds"])
-                        real_values[consts.PULLS_INDEX] = float(cv_data["num_pulls"])
-                        real_values[consts.STEEL_START_INDEX] = cv_data["overall"]["steel_pct_start"]
-                        real_values[consts.STEEL_END_INDEX] = cv_data["overall"]["steel_pct_end"]
-                        real_values[consts.TOTAL_SLAG_START_INDEX] = cv_data["overall"]["total_slag_pct_start"]
-                        real_values[consts.TOTAL_SLAG_END_INDEX] = cv_data["overall"]["total_slag_pct_end"]
-                        real_values[consts.SOLID_SLAG_START_INDEX] = cv_data["overall"]["solid_slag_pct_start"]
-                        real_values[consts.SOLID_SLAG_END_INDEX] = cv_data["overall"]["solid_slag_pct_end"]
-                        real_values[consts.LIQUID_SLAG_START_INDEX] = cv_data["overall"]["liquid_slag_pct_start"]
-                        real_values[consts.LIQUID_SLAG_END_INDEX] = cv_data["overall"]["liquid_slag_pct_end"]
-                        real_values[consts.SLAG_START_INDEX] = cv_data["overall"]["slag_index_start"]
-                        real_values[consts.SLAG_END_INDEX] = cv_data["overall"]["slag_index_end"]
-                        print(f"Raking for heat id {cv_data["heat_id"]} took {cv_data["total_time_seconds"]}s and required {cv_data["num_pulls"]} pulls")
-                        cv_data = {}
-
-                    elif camera_data:
-                        real_values[consts.CAMERA_TEMPERATURE_INDEX] = camera_data["temperature"]
-                        bool_values[consts.CAMERA_STATUS_INDEX] = camera_data["connected"]
-                        camera_data = {}
-
-                    # Write back
-                    await data.set_value(real_values, ua.VariantType.Double)
-                    await flags.set_value(bool_values, ua.VariantType.Boolean)
-                except Exception as e:
-                    print(f"Failed to write: {e}")
-                await asyncio.sleep(1)
-    finally:
-        mqtt_client.loop_stop()
-        mqtt_client.disconnect()
+            await subscription.subscribe_data_change(states_node)
+            await handle_mqtt_messages(mqtt_client, data_node, flags_node)
 
 if __name__ == "__main__":
-    asyncio.run(main())
-
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        pass
