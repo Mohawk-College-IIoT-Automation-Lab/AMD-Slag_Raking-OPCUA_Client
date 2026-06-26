c458502370816cdbeb122ffbe882621ec31b8a1c
daniil <danil-bryzgalov@mohawkcollege.ca>
2026-06-26 10:39:16 -0400

    Change camera topic to match prod

--- middleware-client.py
+++ middleware-client.py
@@ -77,8 +77,6 @@ async def handle_mqtt_messages(mqtt_client: aiomqtt.Client, data_nodes: list[Nod
 
             elif message.topic.value == consts.CAMERA_TOPIC:
                     process_results[consts.CAMERA_TEMPERATURE_INDEX] = mqtt_data["temperature"]
-                    camera_connected = mqtt_data["connected"]
-                    await camera_connection_node.set_value(value=camera_connected, varianttype=ua.VariantType.Boolean)
 
             for idx, result in enumerate(process_results):
                 await data_nodes[idx].set_value(value=result, varianttype=ua.VariantType.Double)
